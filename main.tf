91bc7073bb2a2c967b791a468a0841c169df5890
paladugubharath <2006bharathchowdary@gmail.com>
2026-03-04 11:42:23 +0530

    Updated main.tf and improved gitignore

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 provider "aws" {
   region = "eu-north-1"
 }
-*/
+
 # ---------------- VPC ----------------
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -92,4 +92,53 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Terraform-Server"
   }
+}
+*/
+ 
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-056335ec4a8783947" 
+  instance_type = "t3.micro"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install nginx -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
 }
